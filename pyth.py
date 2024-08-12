2c167018186f76d4cbab4ef48bad777dba060145
Rahul-shingade <rahulshingade225@gmail.com>
2024-08-12 22:23:27 +0530

    ""

--- pyth.py
+++ pyth.py
@@ -1,1 +1,6 @@
-print("rahul hello world")
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+my_pipeline = Pipeline([
+    ('imputer' ,SimpleImputer(strategy ='median')),
+    ('std_sclar', StandardScaler()),
+])
