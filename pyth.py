ef43b8c7b66d9528d4ce8e1d5aa4c5f5792496e9
Rahul-shingade <rahulshingade225@gmail.com>
2024-08-20 19:24:54 +0530

    Real Estate prize predict

--- pyth.py
+++
@@ -1,6 +0,0 @@
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-my_pipeline = Pipeline([
-    ('imputer' ,SimpleImputer(strategy ='median')),
-    ('std_sclar', StandardScaler()),
-])
